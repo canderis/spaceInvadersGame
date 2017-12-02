cc39953df854bb0f7736afecdb67de10ae12c73d
Scott Huffman <scott@shuffman.com>
2017-12-01 18:06:13 -0600

    Shields Changed spawning layout

--- main.cpp
+++ main.cpp
@@ -22,11 +22,15 @@ bool ai(int posX, int posY){
     }
 
     if(doAi % 2000 == 0){
-        mvaddstr(21, 5, std::to_string(doAi).c_str());
 
         if(posY + 1 >= 20)
             return false;
         posY++;
+    }
+    if (rand() % 100 + 1 == 1){
+        board[posX][posY + 1] = '*';
+        aiDone[posX][posY + 1] = true;
+
     }
 
     board[posX][posY] = 'V';
@@ -51,8 +55,16 @@ int main(int argc, char *argv[]){
     int pcX = 40;
     for(int i = 0; i < X; i++){
         for(int j = 0; j < Y; j++ ){
-            if(i > 20 && i < 60 && j > 5 && j < 10 && ( (i % 2 == 0 && j % 2 == 0) ) ){
+            // if(i > 20 && i < 60 && j == 5 && i % 2 == 0 ){
+            //     board[i][j] = 'V';
+            // }
+            // else if(i > 20 && i < 60 && j == 7 && i % 2 != 0 ){
+            //     board[i][j] = 'V';
+            //
+            // }
+            if(i > 20 && i < 60 && j == 9 && i % 2 == 0 ){
                 board[i][j] = 'V';
+
             }
             else if(i == pcX && j == 20){
                 board[i][j] = '^';
@@ -89,6 +101,7 @@ int main(int argc, char *argv[]){
         for(int i = 0; i < X; i++){
             for(int j = 0; j < Y; j++ ){
                 if(doAi % 100 == 0){
+
                     if(board[i][j] == 'V' && !aiDone[i][j]){
                         if(ai(i,j) == false){
                             run = false;
@@ -103,23 +116,19 @@ int main(int argc, char *argv[]){
                                 board[i][j-1] = '.';
                         }
                     }
-                    else if(board[i][j] == '*' ){
-                        board[i][j] = ' ';
-                        if(j + 1 >= 20){
-                            if(board[i][j-1] == '^')
-                                board[i][j-1] = '#';
-                            else
-                                board[i][j-1] = '*';
+                    else if(board[i][j] == '*' && !aiDone[i][j] ){
+                        aiDone[i][j+1] = true;
+                        if(j + 1 <= 20){
+                            if(board[i][j+1] != 'V')
+                                board[i][j+1] = '*';
                         }
                     }
                     else if(board[i][j] == '#'){
                         board[i][j] = ' ';
                     }
+
                 }
-                if(doAi % 327 == 0 && board[i][j] == ' ' && j - 1 >= 0 && board[i][j-1] == 'V'){
-                    board[i][j] = '*';
-                    doAi++;
-                }
+
 
                 mvaddch(j, i, board[i][j]);
             }
