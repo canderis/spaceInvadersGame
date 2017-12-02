ac1ac19cdb9d3c40026dba890261c96a9de4fa32
Scott Huffman <scott@shuffman.com>
2017-12-01 18:40:33 -0600

    Win/loss screen Play again

--- main.cpp
+++ main.cpp
@@ -49,94 +49,136 @@ void resetAiBoard(){
 }
 
 int main(int argc, char *argv[]){
-    initscr();                 /* Start curses mode     */
-    timeout(1);
+    initscr();
+    curs_set(0);
+    bool playAgain;
+    do{
+        timeout(1);
+        playAgain = false;
 
-    int pcX = 40;
-    for(int i = 0; i < X; i++){
-        for(int j = 0; j < Y; j++ ){
-            // if(i > 20 && i < 60 && j == 5 && i % 2 == 0 ){
-            //     board[i][j] = 'V';
-            // }
-            // else if(i > 20 && i < 60 && j == 7 && i % 2 != 0 ){
-            //     board[i][j] = 'V';
-            //
-            // }
-            if(i > 20 && i < 60 && j == 9 && i % 2 == 0 ){
-                board[i][j] = 'V';
-
-            }
-            else if(i == pcX && j == 20){
-                board[i][j] = '^';
-            }
-            else{
-                board[i][j] = ' ';
-            }
-        }
-    }
-    bool run = true;
-    while(run){
-        doAi++;
-        if(doAi % 1000 == 0){
-            right = !right;
-        }
-        resetAiBoard();
-
-        int key;
-        switch (key = getch()) {
-            case 'C':
-                board[pcX++][20] = ' ';
-                board[pcX][20] = '^';
-                break;
-            case 'D':
-                board[pcX--][20] = ' ';
-                board[pcX][20] = '^';
-                break;
-            case 'A': //bullet
-                board[pcX][19] = '.';
-                break;
-            case 'q':
-                run = false;
-        }
+        int pcX = 40;
         for(int i = 0; i < X; i++){
             for(int j = 0; j < Y; j++ ){
-                if(doAi % 100 == 0){
-
-                    if(board[i][j] == 'V' && !aiDone[i][j]){
-                        if(ai(i,j) == false){
-                            run = false;
-                        }
-                    }
-                    else if(board[i][j] == '.' ){
-                        board[i][j] = ' ';
-                        if(j - 1 >= 0){
-                            if(board[i][j-1] == 'V')
-                                board[i][j-1] = '#';
-                            else
-                                board[i][j-1] = '.';
-                        }
-                    }
-                    else if(board[i][j] == '_' && !aiDone[i][j] ){
-                        aiDone[i][j+1] = true;
-                        if(j + 1 <= 20){
-                            if(board[i][j+1] != 'V')
-                                board[i][j+1] = '_';
-                        }
-                    }
-                    else if(board[i][j] == '#'){
-                        board[i][j] = ' ';
-                    }
+                // if(i > 20 && i < 60 && j == 5 && i % 2 == 0 ){
+                //     board[i][j] = 'V';
+                // }
+                // else if(i > 20 && i < 60 && j == 7 && i % 2 != 0 ){
+                //     board[i][j] = 'V';
+                //
+                // }
+                if(i > 20 && i < 60 && j == 9 && i % 2 == 0 ){
+                    board[i][j] = 'V';
 
                 }
-
-
-                mvaddch(j, i, board[i][j]);
+                else if(i == pcX && j == 20){
+                    board[i][j] = '^';
+                }
+                else{
+                    board[i][j] = ' ';
+                }
             }
         }
+        bool run = true;
+        bool enemiesRemaining = true;
 
-        refresh();                 /* Print it on to the real screen */
-    }
-    endwin();                  /* End curses mode    */
+        while(run){
+            doAi++;
+            if(doAi % 1000 == 0){
+                right = !right;
+            }
+            resetAiBoard();
+
+            int key;
+            switch (key = getch()) {
+                case 'C':
+                    board[pcX++][20] = ' ';
+                    board[pcX][20] = '^';
+                    break;
+                case 'D':
+                    board[pcX--][20] = ' ';
+                    board[pcX][20] = '^';
+                    break;
+                case 'A': //bullet
+                    board[pcX][19] = '.';
+                    break;
+                case 'q':
+                    run = false;
+            }
+            if(doAi % 100==0){
+                enemiesRemaining = false;
+            }
+            for(int i = 0; i < X; i++){
+                for(int j = 0; j < Y; j++ ){
+                    if(doAi % 100 == 0){
+
+                        if(board[i][j] == 'V' && !aiDone[i][j]){
+                            enemiesRemaining = true;
+                            if(ai(i,j) == false){
+                                run = false;
+                            }
+                        }
+                        else if(board[i][j] == '.' ){
+                            board[i][j] = ' ';
+                            if(j - 1 >= 0){
+                                if(board[i][j-1] == 'V')
+                                    board[i][j-1] = '#';
+                                else
+                                    board[i][j-1] = '.';
+                            }
+                        }
+                        else if(board[i][j] == '_' && !aiDone[i][j] ){
+                            aiDone[i][j+1] = true;
+                            if(j + 1 <= 20){
+                                if(board[i][j+1] != 'V')
+                                    board[i][j+1] = '_';
+                            }
+                        }
+                        else if(board[i][j] == '#'){
+                            board[i][j] = ' ';
+                        }
+
+                    }
+
+
+                    mvaddch(j, i, board[i][j]);
+                }
+
+            }
+
+            if(enemiesRemaining == false){
+                run = false;
+            }
+
+            refresh();
+        }
+
+        clear();
+        timeout(-1);
+
+        mvaddstr(10,35, "Game Over");
+        if(enemiesRemaining){
+            mvaddstr(12,35, "You lost.");
+        }
+        else{
+            mvaddstr(12,35, "You won!!");
+
+
+        }
+        mvaddstr(14,29, "Press y to play again.");
+        do{
+            if(!(getch() == 'A' || getch() == 'C' || getch() == 'D')){
+                if(getch() == 'y'){
+                    playAgain = true;
+                }
+                break;
+            }
+        }while(true);
+
+
+    }while(playAgain);
+
+
+    endwin();
 
     return 0;
 }
